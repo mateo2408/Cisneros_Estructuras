3cca7de78ba7823b32d4e9431176372535d0bbfd
Mateo Cisneros <mateo.cisneros@udla.edu.ec>
2024-01-16 14:55:50 -0500

    Primer commit

---
+++ Base_De_Datos.c
@@ -0,0 +1,7 @@
+/*Cada esudiante debe trabajar en el ejemplo de estructuras de la clase anterior (alumno)
+y debe crear un programa que permita al usuario el ingreso de n alumnos
+funciones:
+gets();
+puts();
+fflush();
+Nota: No quemas los datos en el codigo*/
