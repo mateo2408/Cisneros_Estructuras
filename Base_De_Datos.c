1c2553bced8d6a4f8304686da71b638e6a2ec808
Mateo Cisneros <mateo.cisneros@udla.edu.ec>
2024-01-16 15:03:01 -0500

    Creacion struct y mnesajes de datos

--- Base_De_Datos.c
+++ Base_De_Datos.c
@@ -5,3 +5,41 @@ gets();
 puts();
 fflush();
 Nota: No quemas los datos en el codigo*/
+#include<stdio.h>
+
+struct alumno
+{
+    int matricula;
+    char nombre[50];
+    char direccion[50];
+    char materia[50];
+    float nota;
+};
+
+int main()
+{
+    int cantidad;
+    printf("Ingrese el numero de alumnos: ");
+    scanf("%d",&cantidad);
+    struct alumno alumnos[cantidad];
+    printf("Ingrese los datos de los alumnos\n");
+    
+    for (int i = 0; i < cantidad; i++)
+    {
+        printf("Ingrese los datos del alumno %d\n",i+1);
+        printf("Matricula: ");
+        gets(alumnos[i].matricula);
+        printf("Nombre: ");
+        gets(alumnos[i].nombre);
+        printf("Direccion: ");
+        gets(alumnos[i].direccion);
+        printf("Materia: ");
+        gets(alumnos[i].materia);
+        printf("Nota: ");
+        scanf("%f",&alumnos[i].nota);
+    }
+    
+    
+    return 0;
+}
+
